d2cf382af2ee5dc7c4338aed57ee83c302ee7edb
Daren Jacobs <darenjacobs@msn.com>
2024-04-15 22:09:42 -0400

    OOP student

--- student2.cpp
+++ student2.cpp
@@ -3,16 +3,19 @@
 
 using namespace std;
 
-class Student // base class
-{
+// Base class representing a Student
+class Student {
+    // Instance variables
     string name;
     string campus;
     string d_o_b; // date of birth
     string issue_date;
     char gender;
     int id;
+
 public:
-    Student(string name, string campus, string d_o_b, string issue_date, char gender, int id ) {
+    // Constructor
+    Student(string name, string campus, string d_o_b, string issue_date, char gender, int id) {
         setName(name);
         setCampus(campus);
         setDob(d_o_b);
@@ -20,14 +23,16 @@ public:
         setGender(gender);
         setId(id);
     }
-    ~Student(){}
+
+    // Destructor
+    ~Student() {}
 
     // Getters
     string getName() { return name; }
     string getCampus() { return campus; }
     string getDob() { return d_o_b; }
     string getIssueDate() { return issue_date; }
-    char getGender() {return gender; }
+    char getGender() { return gender; }
     int getId() { return id; }
 
     // Setters
@@ -40,52 +45,64 @@ public:
 
     // Methods
     void display() {
-        cout << "Name: "  << getName() << endl;
-        cout << "Campus: "  << getCampus() << endl;
-        cout << "Date of Birth: "  << getDob() << endl;
-        cout << "Issue Date: "  << getIssueDate() << endl;
-        cout << "Gender: "  << getGender() << endl;
-        cout << "Student ID Number: "  << getId() << endl;
+        cout << "Name: " << getName() << endl;
+        cout << "Campus: " << getCampus() << endl;
+        cout << "Date of Birth: " << getDob() << endl;
+        cout << "Issue Date: " << getIssueDate() << endl;
+        cout << "Gender: " << getGender() << endl;
+        cout << "Student ID Number: " << getId() << endl;
     }
-    void printAbout()
-    {
+
+    void printAbout() {
         cout << "I am a student" << endl;
     }
-
 };
 
-class Undergraduate : public Student  // derived class
-{
-private:
-    string year;  // Freshmen Sophomore Junior Senior
+// Derived class representing an Undergraduate student
+class Undergraduate : public Student {
+    // Instance variable
+    string year; // Freshmen Sophomore Junior Senior
+
 public:
-    Undergraduate(string year, string name, string campus, string d_o_b, string issue_date, char gender, int id )
-    : Student(name, campus, d_o_b, issue_date, gender, id) {
+    // Constructor
+    Undergraduate(string year, string name, string campus, string d_o_b, string issue_date, char gender, int id)
+        : Student(name, campus, d_o_b, issue_date, gender, id) {
         setYear(year);
     }
+
+    // Setter
     void setYear(string year) { this->year = year; }
+
+    // Getter
     string getYear() { return year; }
 
-    // derived class methods
+    // Derived class methods
     void display() {
         cout << "Year: " << getYear() << endl;
         Student::display();
     }
-    void printAbout()
-    {
+
+    void printAbout() {
         cout << "I am an Undergraduate" << endl;
     }
 };
 
-int main()
-{
-        Student s1("James Brown","Busch","10-12-2001","02-15-2022",'M',100012345);
-        Undergraduate u1("Freshman","Michael Oliver","York","04-22-2003","09-02-2024",'M',100012346);
-        cout << "\n";
-        s1.printAbout();
-        s1.display();
-        cout << "----------------\n";
-        u1.printAbout();
-        u1.display();
-        return  0;
+int main() {
+    // Create a Student object
+    Student s1("James Brown", "Busch", "10-12-2001", "02-15-2022", 'M', 100012345);
+
+    // Create an Undergraduate object
+    Undergraduate u1("Freshman", "Michael Oliver", "York", "04-22-2003", "09-02-2024", 'M', 100012346);
+
+    // Print information about the Student object
+    cout << "\n";
+    s1.printAbout();
+    s1.display();
+    cout << "----------------\n";
+
+    // Print information about the Undergraduate object
+    u1.printAbout();
+    u1.display();
+
+    return 0;
 }
