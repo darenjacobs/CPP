51781d3759692e92c000adc80d7891acc1e9ed71
Daren Jacobs <darenjacobs@msn.com>
2024-05-02 15:11:13 -0400

    space change
    Changed the spaces from 8 spaces to 4 spaces

--- inheritence.cpp
+++ inheritence.cpp
@@ -2,117 +2,117 @@
 using namespace std;
 class HomePet {
 protected:
-        string color;
-        string name;
-        int age;
+    string color;
+    string name;
+    int age;
 public:
-        HomePet() {
-                setColor("Unknown");
-                setName("Unknown");
-                setAge(0);
-        }
-        HomePet(string color, string name, int age) {
-                setColor(color);
-                setName(name);
-                setAge(age);
-        }
-        ~HomePet(){}
-        void setColor(string color) {this->color = color;}
-        void setName(string name) {this->name = name;}
-        void setAge(int age) {this->age = age;}
-        string getColor() {return this->color;}
-        string getName() {return this->name;}
-        int getAge() {return this->age;}
-        void display() {
-                cout << "Name: " << getName() << endl;
-                cout << "Color: " << getColor() << endl;
-                cout << "Age: " << getAge() << endl;
-        }
-        void breath() {
-                cout << "nspiration occurs when the diaphram and the external intercostal muscles contract.  Expiration occurs when the diaphram and the intercostal muscles relax.\n" << endl;
-        }
+    HomePet() {
+        setColor("Unknown");
+        setName("Unknown");
+        setAge(0);
+    }
+    HomePet(string color, string name, int age) {
+        setColor(color);
+        setName(name);
+        setAge(age);
+    }
+    ~HomePet(){}
+    void setColor(string color) {this->color = color;}
+    void setName(string name) {this->name = name;}
+    void setAge(int age) {this->age = age;}
+    string getColor() {return this->color;}
+    string getName() {return this->name;}
+    int getAge() {return this->age;}
+    void display() {
+        cout << "Name: " << getName() << endl;
+        cout << "Color: " << getColor() << endl;
+        cout << "Age: " << getAge() << endl;
+    }
+    void breath() {
+        cout << "Inspiration occurs when the diaphragm and the external intercostal muscles contract.  Expiration occurs when the diaphragm and the intercostal muscles relax.\n" << endl;
+    }
 };
 
 class Dog:public HomePet {
 private:
-        string earsSize; // small, mediumm, large
+    string earsSize; // small, medium, large
 public:
-        Dog(string earsSize, string color, string name, int age) {
-                setEarsSize(earsSize);
-                // You can set age color and name in several ways.  See the Bird function below.
-                // it looks like this: Bird(double beakLength, string color, string name, int age):HomePet(color,name,age) {
+    Dog(string earsSize, string color, string name, int age) {
+        setEarsSize(earsSize);
+        // You can set age color and name in several ways.  See the Bird function below.
+        // it looks like this: Bird(double beakLength, string color, string name, int age):HomePet(color,name,age) {
 
-                // You can set name color and age as follows:
-                this->setAge(age);
-                this->setColor(color);
-                this->setName(name);
+        // You can set name color and age as follows:
+        this->setAge(age);
+        this->setColor(color);
+        this->setName(name);
 
-                // You can set name, color and age as follows:
-                // this->name = name;
-                // this->age = age;
-                // this->color = color;
-        }
-        ~Dog() {}
-        void setEarsSize(string earsSize){this->earsSize = earsSize;}
-        string getEarsSize(){return earsSize;}
-        void display() {
-                HomePet::display();
-                cout << "Ears Size: " << getEarsSize() << endl;
-        }
+        // You can set name, color and age as follows:
+        // this->name = name;
+        // this->age = age;
+        // this->color = color;
+    }
+    ~Dog() {}
+    void setEarsSize(string earsSize){this->earsSize = earsSize;}
+    string getEarsSize(){return earsSize;}
+    void display() {
+        HomePet::display();
+        cout << "Ears Size: " << getEarsSize() << endl;
+    }
 };
 
 class Bird:public HomePet {
 private:
-        double beakLength;
+    double beakLength;
 public:
-        Bird(double beakLength, string color, string name, int age):HomePet(color,name,age) {
-                setBeakLength(beakLength);
-        }
-        ~Bird(){}
-        void setBeakLength(double beakLength) {this->beakLength = beakLength;}
-        double getBeakLength() {return beakLength;}
-        void display() {
-                HomePet::display();
-                cout <<  "beak Length: " << getBeakLength() << endl;
-        }
+    Bird(double beakLength, string color, string name, int age):HomePet(color,name,age) {
+        setBeakLength(beakLength);
+    }
+    ~Bird(){}
+    void setBeakLength(double beakLength) {this->beakLength = beakLength;}
+    double getBeakLength() {return beakLength;}
+    void display() {
+        HomePet::display();
+        cout <<  "Beak Length: " << getBeakLength() << endl;
+    }
 };
 
 class Fish:public HomePet {
 private:
-        double finLength;
+    double finLength;
 public:
-        Fish(double finLength, string color, string name, int age):HomePet(color,name,age) {
-                setFinLength(finLength);
-        }
-        ~Fish(){}
-        void setFinLength(double finLength) {this->finLength = finLength;}
-        double getFinLength(){return finLength;}
-        void display() {
-                HomePet::display();
-                cout <<  "Fin Length: " << getFinLength() << endl;
-        }
-        void breath() {
-                cout << "Gills are tissues made up of feathery structures called gill filaments that provide a large surface area for gas exchange.  Fish take in oxygen-rich water through their mouths and pump it over their gills\n" << endl;
-        }
+    Fish(double finLength, string color, string name, int age):HomePet(color,name,age) {
+        setFinLength(finLength);
+    }
+    ~Fish(){}
+    void setFinLength(double finLength) {this->finLength = finLength;}
+    double getFinLength(){return finLength;}
+    void display() {
+        HomePet::display();
+        cout <<  "Fin Length: " << getFinLength() << endl;
+    }
+    void breath() {
+        cout << "Gills are tissues made up of feathery structures called gill filaments that provide a large surface area for gas exchange.  Fish take in oxygen-rich water through their mouths and pump it over their gills\n" << endl;
+    }
 
 };
 
 int main() {
-        // Set the animal properties
-        Dog d1("small","black","Nick",7);
-        Bird b1(1.02,"yellow","Snoop",3);
-        Fish f1(2.25,"gold","Nemo",1);
+    // Set the animal properties
+    Dog d1("small","black","Nick",7);
+    Bird b1(1.02,"yellow","Snoop",3);
+    Fish f1(2.25,"gold","Nemo",1);
 
-        // display the animal properties
-        d1.display();
-        d1.breath();
-        cout << "----------------------------\n";
-        b1.display();
-        b1.breath();
-        cout << "----------------------------\n";
-        f1.display();
-        f1.breath();
-        cout << "Press Enter to Exit...";
-        getchar();
-        return 0;
+    // display the animal properties
+    d1.display();
+    d1.breath();
+    cout << "----------------------------\n";
+    b1.display();
+    b1.breath();
+    cout << "----------------------------\n";
+    f1.display();
+    f1.breath();
+    cout << "Press Enter to Exit...";
+    getchar();
+    return 0;
 }
