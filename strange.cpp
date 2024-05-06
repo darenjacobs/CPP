856bd00bbc3dbb04e925535a66cbdb8c47da96f8
Daren Jacobs <darenjacobs@msn.com>
2024-05-06 17:44:01 -0400

    update spaces

--- strange.cpp
+++ strange.cpp
@@ -4,9 +4,8 @@ using namespace std;
 
 int main()
 {
-	double price = 4.35;
-	int cents = 100 * price; // should be 100 * 4.35 = 435
-	cout << cents << endl; // Prints 434!
-	return 0;
+   	double price = 4.35;
+   	int cents = 100 * price; // should be 100 * 4.35 = 435
+   	cout << cents << endl; // Prints 434!
+   	return 0;
 }
-
